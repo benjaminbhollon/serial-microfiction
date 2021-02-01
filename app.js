c83a5dde038d37b5f745ab243a33f074571bbf9e
Benjamin Hollon <benjaminbhollon@gmail.com>
2021-02-01 19:54:10 +0800

    More debug

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.post('/admin/flash/', async (request, response) => {
   const daysToAdd = ((config.releasedOn.find(day => day > lastPosted.getUTCDay()) === undefined ? config.releasedOn[0] : config.releasedOn.find(day => day > lastPosted.getUTCDay())) - lastPosted.getUTCDay() + 7) % 7;
   const nextPostDue = new Date(lastPosted.getUTCFullYear(), lastPosted.getUTCMonth(), lastPosted.getUTCDate() + daysToAdd + 1);
 
-  console.log("Right now is:", new Date(), "The last post was at:", lastPosted, "We need to add n days:", daysToAdd, "So the next post should be at:", nextPostDue);
+  console.log("Right now is:\n", new Date(), "The last post was at:\n", lastPosted, "We need to add n days:\n", daysToAdd, "So the next post should be at:\n", nextPostDue, "The config is:\n", config);
 
   let flash = {
     date: nextPostDue.toISOString().split("T")[0],
